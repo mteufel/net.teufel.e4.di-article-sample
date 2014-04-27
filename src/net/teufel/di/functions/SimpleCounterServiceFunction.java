afe987101d1bc7101f4155e697ff587cf7f8c466
mteufel <teufel.marc@gmail.com>
2014-04-27 13:49:08 +0200

    benutzt absofort ContextInjectionFactory.make

--- src/net/teufel/di/functions/SimpleCounterServiceFunction.java
+++ src/net/teufel/di/functions/SimpleCounterServiceFunction.java
@@ -3,13 +3,16 @@ package net.teufel.di.functions;
 import net.teufel.di.services.CountBy10Service;
 
 import org.eclipse.e4.core.contexts.ContextFunction;
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 
 public class SimpleCounterServiceFunction extends ContextFunction {
 
 	@Override
 	public Object compute(IEclipseContext context, String contextKey) {
-		return new CountBy10Service();
+		
+		return ContextInjectionFactory.make(CountBy10Service.class, context);
+	
 	}
 	
 }
